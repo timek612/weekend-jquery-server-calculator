98d7e443530cbea40f3fe0eb9becb4b87c3f83e1
AJ Timek <Timek612@gmail.com>
2022-08-02 11:48:03 -0500

    changed port for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 let total = 0;
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended: true}));
 
